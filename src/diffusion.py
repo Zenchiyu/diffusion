Fixed add_noise git add src/ I didn't add correctly the noise. I broadcasted the same noise to every pixel and every channel!

src/diffusion.py
@@ -21,15 +21,15 @@ class Diffusion:
 
     @staticmethod
     def add_noise(x: torch.FloatTensor,
-                  noise_level: float|torch.FloatTensor):
+                  noise_level: torch.FloatTensor) -> torch.FloatTensor:
         # noising procedure using re-param trick
-        return x + noise_level*torch.normal(0, 1, size=noise_level.shape,
-                                            device=x.device)
+        return x + noise_level*torch.randn(size=x.shape, device=x.device)
+    #torch.randn(size=noise_level.shape, device=x.device)
 
     def sample_sigma(self,
                      n: list[int]|tuple[int]|int,
                      loc: float=-1.2,
-                     scale: float=1.2):
+                     scale: float=1.2) -> torch.FloatTensor:
         """
         Sample noise levels following a log-normal distribution.
         These noise levels will be used in the training phase,
@@ -39,7 +39,9 @@ class Diffusion:
         # exp(Z) where Z follows a normal distribution then clip
         return (torch.randn(n, device=self.device) * scale + loc).exp().clip(self.sigma_min, self.sigma_max)
 
-    def build_sigma_schedule(self, steps, rho=7):
+    def build_sigma_schedule(self,
+                             steps: int,
+                             rho: float|int=7) -> torch.FloatTensor:
         """
         Build sigma schedule of decreasing noise levels
         that will be used in the sampling procedure, not training phase.

src/sampler.py
@@ -1,7 +1,6 @@
 # Made by Stephane Nguyen following
 # notebooks/instructions.ipynb
 import matplotlib.pyplot as plt
-import os
 import torch
 from diffusion import Diffusion
 from init import init
@@ -9,64 +8,80 @@ from tqdm import tqdm
 from PIL import Image
 from torchvision.utils import make_grid
 
-import wandb
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 
 def sample(num_samples: int,
+           image_channels: int,
            model: torch.nn.Module,
-           diffusion: Diffusion) -> torch.tensor:
+           diffusion: Diffusion,
+           num_steps: int=50,
+           track_inter: bool=False) -> torch.Tensor:
     model.eval()
-    sigmas = diffusion.build_sigma_schedule(steps=50, rho=7)  # Sequence of decreasing sigmas
+    sigmas = diffusion.build_sigma_schedule(steps=num_steps, rho=7)  # Sequence of decreasing sigmas
     cin = diffusion.cin
     cout = diffusion.cout
     cskip = diffusion.cskip
     cnoise = diffusion.cnoise
     D = lambda X_noisy, sigma: cskip(sigma)*X_noisy+cout(sigma)*model(cin(sigma)*X_noisy, cnoise(sigma))
 
-    X_noisy = torch.randn(num_samples, 1, 32, 32, device=diffusion.device) * sigmas[0]  # Initialize with pure gaussian noise ~ N(0, sigmas[0])
+    X_noisy = torch.randn(num_samples, image_channels,
+                          32, 32, device=diffusion.device) * sigmas[0]  # Initialize with pure gaussian noise ~ N(0, sigmas[0])
+    
+    # Track the iterative procedure
+    X_inter = torch.zeros(size=(len(sigmas)+1, ) + X_noisy.shape)
+    X_inter[0] = X_noisy
+
     for i, sigma in enumerate(sigmas):
         with torch.no_grad():
             X_denoised = D(X_noisy, sigma)  # based on our model, try to denoise X_noisy
         
         sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
-        d = (X_noisy - X_denoised) / sigma
-        
+        d = (X_noisy - X_denoised) / sigma  # derivative
         X_noisy = X_noisy + d * (sigma_next - sigma)  # Perform one step of Euler's method
+
+        # Track the iterative procedure
+        X_inter[i+1] = X_noisy
+    
     model.train()
+    if track_inter:
+        return X_noisy, X_inter
     # Final X_noisy contains the sampled images
     return X_noisy
 
-def display(x: torch.tensor) -> None:
+def float2uint8(x: torch.Tensor) -> torch.Tensor:
     # Clamp/clip and convert to displayable format
-    x = x.clamp(-1, 1).add(1).div(2).mul(255).byte()  # [-1., 1.] -> [0., 1.] -> [0, 255]
-    x = make_grid(x)
+    return x.clamp(-1, 1).add(1).div(2).mul(255).byte()  # [-1., 1.] -> [0., 1.] -> [0, 255]
+
+def display(x: torch.Tensor) -> None:
+    x = make_grid(float2uint8(x))
     x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
     plt.imshow(x)
+    plt.show()
 
-def save(x: torch.tensor, save_path: str) -> None:
-    # Clamp/clip and convert to displayable format
-    x = x.clamp(-1, 1).add(1).div(2).mul(255).byte()  # [-1., 1.] -> [0., 1.] -> [0, 255]
-    x = make_grid(x)
+def save(x: torch.Tensor, save_path: str) -> None:
+    x = make_grid(float2uint8(x))
     x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
     x.save(save_path)
 
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def sampler(cfg: DictConfig):
-    if cfg.wandb.mode == "online":
-        run = wandb.init(config=OmegaConf.to_container(cfg, resolve=True),
-                         **cfg.wandb)
-    
-    model, _, _, diffusion, dl, _, device, save_path, chkpt_path = init(cfg)
+    model, _, _, diffusion, dl, info, device, save_path, chkpt_path = init(cfg)
 
     # Load model state dict from checkpoint:
     chkpt = torch.load(chkpt_path)
     model.load_state_dict(chkpt["model_state_dict"])
 
     # Sample and display
-    num_samples = 8
-    samples = sample(num_samples, model, diffusion)
+    num_samples = 1
+    samples, samples_inter = sample(num_samples,
+                                    info.image_channels,
+                                    model,
+                                    diffusion,
+                                    num_steps=cfg.common.sampling.num_steps,
+                                    track_inter=True)
     display(samples)
+    display(samples_inter.view(-1, info.image_channels, 32, 32))
 
 if __name__ == "__main__":
     sampler()

src/trainer.py
@@ -15,6 +15,7 @@ def trainer(cfg: DictConfig):
     print("Config:")
     print(OmegaConf.to_yaml(cfg))
     
+    # TODO: work with same dataset!
     model, optimizer, criterion, diffusion, dl, info, device, save_path, chkpt_path = init(cfg)
     print(f"\n\nDataset: {cfg.dataset.name}, Using device: {device}")
     
@@ -63,7 +64,9 @@ def trainer(cfg: DictConfig):
             acc_loss += loss.item()
 
         acc_losses.append(acc_loss)
-        samples = sample(8, model, diffusion)  # it's switching between eval and train modes
+        samples = sample(8, info.image_channels,
+                         model, diffusion,
+                         cfg.common.sampling.num_steps)  # it's switching between eval and train modes
         save(samples, str(save_path))
         # Save checkpoint
         torch.save({"nb_epochs_finished": e+1,