b1cbe6d6ef116bf24fe4eff621ecdfeb2e015633
Zenchiyu <stephane.nguyen@etu.unige.ch>
2023-11-17 14:50:48 +0100

    moved some functions from sampler.py to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
+import matplotlib.pyplot as plt
 import os
 import shutil
+import torch
 import wandb
 
 from pathlib import Path
-
+from PIL import Image
+from torchvision.utils import make_grid
 
 def copy_config(run: wandb.sdk.wandb_run.Run,
                 begin_date: str) -> None:
@@ -17,3 +20,21 @@ def copy_chkpt(run: wandb.sdk.wandb_run.Run,
     path = f'checkpoints/{begin_date}/run_{run.id}'
     os.makedirs(path, exist_ok=True)
     shutil.copyfile(chkpt_path, path + "/checkpoint.pth")
+
+def float2uint8(x: torch.Tensor) -> torch.Tensor:
+    # Clamp/clip and convert to displayable format
+    return x.clamp(-1, 1).add(1).div(2).mul(255).byte()  # [-1., 1.] -> [0., 1.] -> [0, 255]
+
+def display(x: torch.Tensor,
+            nrow: int=8) -> None:
+    x = make_grid(float2uint8(x), nrow=nrow)
+    x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
+    plt.imshow(x)
+    plt.show()
+
+def save(x: torch.Tensor,
+         save_path: str,
+         nrow: int=8) -> None:
+    x = make_grid(float2uint8(x), nrow=nrow)
+    x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
+    x.save(save_path)
