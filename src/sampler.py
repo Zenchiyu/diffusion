trying to add a code that works with classifier free guidance. Not sure if correct, I did an interpolation between what I think are the score functions and used it to get the derivative in Euler method

src/sampler.py
@@ -1,97 +1,130 @@
 # Made by Stephane Nguyen following
 # notebooks/instructions.ipynb
 import hydra
-import matplotlib.pyplot as plt
 import torch
 
 from diffusion import Diffusion
 from init import init
+from utils import display
 
 from omegaconf import DictConfig
-from PIL import Image
-from tqdm import tqdm
-from torchvision.utils import make_grid
+from typing import Optional, Callable
 
 
-def sample(num_samples: int,
-           image_channels: int,
-           image_size: int,
-           model: torch.nn.Module,
-           diffusion: Diffusion,
-           num_steps: int=50,
-           track_inter: bool=False) -> torch.Tensor:
-    model.eval()
-    sigmas = diffusion.build_sigma_schedule(steps=num_steps, rho=7)  # Sequence of decreasing sigmas
-    cin = diffusion.cin
-    cout = diffusion.cout
-    cskip = diffusion.cskip
-    cnoise = diffusion.cnoise
-    D = lambda X_noisy, sigma: cskip(sigma)*X_noisy+cout(sigma)*model(cin(sigma)*X_noisy, cnoise(sigma))
+def euler_method(
+        sigmas: torch.Tensor,
+        X_noisy: torch.Tensor,
+        D: Callable[[torch.Tensor, torch.Tensor, Optional[int]], torch.Tensor]
+    ) -> tuple[torch.Tensor, ...]:
 
-    X_noisy = torch.randn(num_samples, image_channels,
-                          image_size, image_size,
-                          device=diffusion.device) * sigmas[0]  # Initialize with pure gaussian noise ~ N(0, sigmas[0])
-    
     # Track the iterative procedure
     X_inter = torch.zeros(size=(len(sigmas)+1, ) + X_noisy.shape)
     X_inter[0] = X_noisy
-
+    
     for i, sigma in enumerate(sigmas):
-        with torch.no_grad():
-            s = sigma.expand(X_noisy.shape[0])
-            X_denoised = D(X_noisy, s)  # based on our model, try to denoise X_noisy
+        s = sigma.expand(X_noisy.shape[0])
+        X_denoised_uncond = D(X_noisy, s, None)  # based on our model, try to denoise X_noisy
         
         sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
-        d = (X_noisy - X_denoised) / sigma  # derivative
+        d = (X_noisy - X_denoised_uncond) / sigma     # derivative
         X_noisy = X_noisy + d * (sigma_next - sigma)  # Perform one step of Euler's method
 
         # Track the iterative procedure
         X_inter[i+1] = X_noisy
+    return X_noisy, X_inter
+
+def euler_method_conditional(
+        sigmas: torch.Tensor,
+        X_noisy: torch.Tensor,
+        D: Callable[[torch.Tensor, torch.Tensor, Optional[int]], torch.Tensor],
+        label: Optional[int]=None,
+        cfg_scale: float=0
+    ) -> tuple[torch.Tensor, ...]:
+
+    # Track the iterative procedure
+    X_inter = torch.zeros(size=(len(sigmas)+1, ) + X_noisy.shape)
+    X_inter[0] = X_noisy
     
+    for i, sigma in enumerate(sigmas):
+        s = sigma.expand(X_noisy.shape[0])
+        X_denoised_uncond = D(X_noisy, s, None)  # based on our model, try to denoise X_noisy
+        X_denoised_cond   = D(X_noisy, s, label)
+
+        sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
+
+        # Classifier-Free Guidance
+        score_uncond_estimate = (X_noisy - X_denoised_uncond)/sigma**2  # of x_noisy
+        score_cond_estimate   = (X_noisy - X_denoised_cond)/sigma**2    # of x_noisy given label
+        s_estimate = torch.lerp(score_uncond_estimate, score_cond_estimate, cfg_scale)
+        
+        d = sigma*s_estimate  # derivative
+        X_noisy = X_noisy + d * (sigma_next - sigma)  # Perform one step of Euler's method
+
+        # Track the iterative procedure
+        X_inter[i+1] = X_noisy
+    return X_noisy, X_inter
+
+def sample(
+        num_samples: int,
+        image_channels: int,
+        image_size: int,
+        model: torch.nn.Module,
+        diffusion: Diffusion,
+        num_steps: int=50,
+        label: Optional[int]=None,
+        num_classes: Optional[int]=None,
+        cfg_scale: float=0,
+        track_inter: bool=False
+    ) -> torch.Tensor:
+    # XXX: If want to apply CFG, need to specify both label and num_classes
+    # XXX: and CFG scale
+    model.eval()
+    with torch.no_grad():
+        sigmas = diffusion.build_sigma_schedule(steps=num_steps, rho=7)  # Sequence of decreasing sigmas
+        cin, cout, cskip, cnoise = diffusion.cin, diffusion.cout, diffusion.cskip, diffusion.cnoise
+        clabel = lambda label: label/num_classes - 0.5 if (label and num_classes) else None # XXX: Karras paper seem to have used
+        # one-hot encoded vectors divided by sqrt(num_classes) before MLP embedding?
+        
+        # Denoiser
+        D = lambda X_noisy, sigma, label: cskip(sigma)*X_noisy+cout(sigma)*model(cin(sigma)*X_noisy, cnoise(sigma), clabel(label))
+
+        # Initialize with pure gaussian noise ~ N(0, sigmas[0])
+        # Initial condition of the differential equation
+        X_noisy = torch.randn(num_samples, image_channels,
+                            image_size, image_size,
+                            device=diffusion.device) * sigmas[0]
+        
+        # TODO: make this cleaner
+        if not(label and num_classes):
+            X_noisy, X_inter = euler_method(sigmas, X_noisy, D)
+        else:
+            X_noisy, X_inter = euler_method_conditional(sigmas, X_noisy, D, label, cfg_scale)
+        
     model.train()
+
     if track_inter:
         return X_noisy, X_inter
     # Final X_noisy contains the sampled images
     return X_noisy
 
-def float2uint8(x: torch.Tensor) -> torch.Tensor:
-    # Clamp/clip and convert to displayable format
-    return x.clamp(-1, 1).add(1).div(2).mul(255).byte()  # [-1., 1.] -> [0., 1.] -> [0, 255]
-
-def display(x: torch.Tensor,
-            nrow: int=8) -> None:
-    x = make_grid(float2uint8(x))
-    x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
-    plt.imshow(x)
-    plt.show()
-
-def save(x: torch.Tensor,
-         save_path: str,
-         nrow: int=8) -> None:
-    x = make_grid(float2uint8(x))
-    x = Image.fromarray(x.permute(1, 2, 0).cpu().numpy())
-    x.save(save_path)
-
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def sampler(cfg: DictConfig):
-    model, _, _, diffusion, dl, info, device, save_path, chkpt_path = init(cfg)
-
-    # Load model state dict from checkpoint:
-    chkpt = torch.load(chkpt_path, map_location=device)
-    model.load_state_dict(chkpt["model_state_dict"])
+    # Initialization
+    init_tuple = init(cfg)
+    model, diffusion, info = init_tuple.model, init_tuple.diffusion, init_tuple.info
 
     # Sample and display
-    num_samples = 8*8
-    samples, samples_inter = sample(num_samples,
-                                    info.image_channels,
-                                    info.image_size,
-                                    model,
-                                    diffusion,
+    N, C, H, W = 8*8, info.image_channels, info.image_size, info.image_size
+    samples, samples_inter = sample(N, C, H, model, diffusion,
                                     num_steps=cfg.common.sampling.num_steps,
+                                    label=cfg.common.sampling.label,
+                                    num_classes=info.num_classes,
+                                    cfg_scale=cfg.common.sampling.cfg_scale,
                                     track_inter=True)
     display(samples)
-    # Display for the first generated picture
-    display(samples_inter[:, 0].view(-1, info.image_channels, 32, 32))
+    # Display intermediate generation steps 
+    # for the first generated picture
+    display(samples_inter[:, 0].view(-1, C, H, W))
 
 if __name__ == "__main__":
     sampler()