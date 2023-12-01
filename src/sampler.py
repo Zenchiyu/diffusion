cd2345e60e8d37ee556711cbe6e91730c858ea12
Zenchiyu <stephane.nguyen@etu.unige.ch>
2023-12-01 20:41:40 +0100

    Fixing seeds & added stochastic sampling with 2nd order Heun (but not completely correct yet)

--- src/sampler.py
+++ src/sampler.py
@@ -1,4 +1,5 @@
 import hydra
+import numpy as np
 import torch
 
 from diffusion import Diffusion
@@ -9,12 +10,19 @@ from omegaconf import DictConfig
 from typing import Optional, Callable
 
 
-def euler_method(
+def expand(sigma: torch.Tensor, num: int) -> torch.Tensor:
+    return sigma.expand(num)
+
+def uncond_method(
         sigmas: torch.Tensor,
         X_noisy: torch.Tensor,
         D: Callable[[torch.Tensor, torch.Tensor, int], torch.Tensor],
         uncond_label: Optional[int],
+        stochastic: bool=False,
+        euler_improved: bool=False,
     ) -> tuple[torch.Tensor, ...]:
+    # N, S_min, S_max, S_churn, S_noise = sigmas.numel(), 0.05, 50, 10, 1.003  # 1.003
+    N, S_min, S_max, S_churn, S_noise = sigmas.numel(), 0.05, 50, 10, 1  # 1.003
 
     if uncond_label: uncond_label = torch.tensor(uncond_label, device=X_noisy.device).expand(X_noisy.shape[0])
 
@@ -22,25 +30,39 @@ def euler_method(
     X_inter = torch.zeros(size=(len(sigmas)+1, ) + X_noisy.shape)
     X_inter[0] = X_noisy
     
+    derivative = lambda x, sigma: (x - D(x, expand(sigma, x.shape[0]), uncond_label)) / sigma
+    
     for i, sigma in enumerate(sigmas):
-        s = sigma.expand(X_noisy.shape[0])
-        X_denoised_uncond = D(X_noisy, s, uncond_label)  # based on our model, try to denoise X_noisy
-        
         sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
-        d = (X_noisy - X_denoised_uncond) / sigma     # derivative
-        X_noisy = X_noisy + d * (sigma_next - sigma)  # Perform one step of Euler's method
+        
+        if stochastic:
+            gamma = np.sqrt(2)-1  # min(S_churn/N, np.sqrt(2)-1) if (sigma >= S_min and sigma <= S_max) else 0   # np.sqrt(2)-1  TODO fix this
+            sigma_new = sigma + gamma*sigma
+            X_noisy = X_noisy + torch.sqrt(sigma_new**2-sigma**2)*torch.randn(X_noisy.shape, device=X_noisy.device)*S_noise  # TODO fix S_noise
+
+            sigma = sigma_new
+
+        d = derivative(X_noisy, sigma)
+        X_next = X_noisy + (sigma_next - sigma)*d  # Perform one step of Euler's method
+
+        if euler_improved and sigma_next != 0:
+            d_next = derivative(X_next, sigma_next)
+            X_next = X_noisy + (sigma_next - sigma)*(d + d_next)/2
+        X_noisy = X_next 
 
         # Track the iterative procedure
         X_inter[i+1] = X_noisy
     return X_noisy, X_inter
 
-def euler_method_conditional(
+def conditional_method(
         sigmas: torch.Tensor,
         X_noisy: torch.Tensor,
         D: Callable[[torch.Tensor, torch.Tensor, int], torch.Tensor],
         label: int,
         uncond_label: int,
-        cfg_scale: float=0
+        cfg_scale: float=0,
+        stochastic: bool=False,
+        euler_improved: bool=False
     ) -> tuple[torch.Tensor, ...]:
     
     label = torch.tensor(label, device=X_noisy.device).expand(X_noisy.shape[0])
@@ -49,22 +71,30 @@ def euler_method_conditional(
     # Track the iterative procedure
     X_inter = torch.zeros(size=(len(sigmas)+1, ) + X_noisy.shape)
     X_inter[0] = X_noisy
-    
-    for i, sigma in enumerate(sigmas):
-        s = sigma.expand(X_noisy.shape[0])
+
+    def derivative(x, sigma):
+        s = expand(sigma, x.shape[0])
         # TODO: concat
-        X_denoised_uncond = D(X_noisy, s, uncond_label)  # based on our model, try to denoise X_noisy
-        X_denoised_cond   = D(X_noisy, s, label)
-
-        sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
-
+        X_denoised_uncond = D(x, s, uncond_label)  # based on our model, try to denoise X_noisy
+        X_denoised_cond   = D(x, s, label)
+        
         # Classifier-Free Guidance
-        score_uncond_estimate = (X_noisy - X_denoised_uncond)/sigma**2  # of x_noisy
-        score_cond_estimate   = (X_noisy - X_denoised_cond)/sigma**2    # of x_noisy given label
+        score_uncond_estimate = (x - X_denoised_uncond)/sigma**2  # of x_noisy
+        score_cond_estimate   = (x - X_denoised_cond)/sigma**2    # of x_noisy given label
         s_estimate = torch.lerp(score_uncond_estimate, score_cond_estimate, cfg_scale)
         
-        d = sigma*s_estimate  # derivative
-        X_noisy = X_noisy + d * (sigma_next - sigma)  # Perform one step of Euler's method
+        return sigma*s_estimate  # derivative
+    
+    for i, sigma in enumerate(sigmas):
+        sigma_next = sigmas[i + 1] if i < len(sigmas) - 1 else 0
+        
+        d = derivative(X_noisy, sigma)
+        X_next = X_noisy + (sigma_next - sigma)*d  # Perform one step of Euler's method
+
+        if euler_improved and sigma_next != 0:
+            d_next = derivative(X_next, sigma_next)
+            X_next = X_noisy + (sigma_next - sigma)*(d + d_next)/2
+        X_noisy = X_next 
 
         # Track the iterative procedure
         X_inter[i+1] = X_noisy
@@ -81,7 +111,8 @@ def sample(
         label: Optional[int]=None,
         cfg_scale: float=0,
         num_steps: int=50,
-        track_inter: bool=False
+        track_inter: bool=False,
+        sampling_method: str="euler"
     ) -> torch.Tensor:
 
     model.eval()
@@ -98,9 +129,13 @@ def sample(
                         device=diffusion.device) * sigmas[0]
     
     if label is None or uncond_label is None:
-        X_noisy, X_inter = euler_method(sigmas, X_noisy, D, uncond_label)
+        X_noisy, X_inter = uncond_method(sigmas, X_noisy, D, uncond_label,
+                                         stochastic="stochastic" in sampling_method,
+                                         euler_improved="heun" in sampling_method)
     else:
-        X_noisy, X_inter = euler_method_conditional(sigmas, X_noisy, D, label, uncond_label, cfg_scale)
+        X_noisy, X_inter = conditional_method(sigmas, X_noisy, D, label, uncond_label, cfg_scale,
+                                              stochastic="stochastic" in sampling_method,
+                                              euler_improved="heun" in sampling_method)
     model.train()
 
     if track_inter:
@@ -110,14 +145,20 @@ def sample(
 
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def sampler(cfg: DictConfig):
+    seed = torch.random.initial_seed()  # retrieve current seed
+
     # Initialization
     init_tuple = init(cfg)
     model, diffusion, info = init_tuple.model, init_tuple.diffusion, init_tuple.info
 
     # Don't use the checkpoint seed for sampling
-    torch.seed()
+    torch.manual_seed(seed)
 
     # Sample and display
+    try:
+        sampling_method = cfg.common.sampling.method
+    except:
+        sampling_method = "euler"
     N, C, H, W = 8*8, info.image_channels, info.image_size, info.image_size
     samples, samples_inter = sample(
             N, C, H, model, diffusion,
@@ -125,7 +166,8 @@ def sampler(cfg: DictConfig):
             label=cfg.common.sampling.label,
             cfg_scale=cfg.common.sampling.cfg_scale,
             num_steps=cfg.common.sampling.num_steps,
-            track_inter=True
+            track_inter=True,
+            sampling_method=sampling_method
         )
     dataset_name = str.lower(cfg.dataset.name)
 
