move generated images. To fix fid computation using clean-fid. Also started running training with the 'positional embedding'. Might not make sense to use different positional embedding for different MHA blocks

src/init.py
@@ -88,13 +88,12 @@ def init(cfg: DictConfig, verbose: bool=True) -> Init:
     # )
     model = UNet(
         image_channels=info.image_channels,     
+        image_size=info.image_size,
         in_channels=cfg.model.nb_channels,
         min_channels=cfg.model.nb_channels,
         depths=cfg.model.depths,
-        # nb_blocks=cfg.model.num_blocks,                         # nb of down + up blocks together
         cond_channels=cfg.model.cond_channels,
         self_attentions=cfg.model.self_attentions,
-        # start_self_attention=cfg.model.start_self_attention,    # index of starting block that contains MHA layers
         nb_classes=info.num_classes                             # will +1 fake label for CFG
     )
     criterion = nn.MSELoss()

src/models/blocks.py
@@ -60,10 +60,9 @@ class CondResidualBlock(CondModule):
         self.norm2 = CondBatchNorm2d(mid_channels, cond_channels)
         self.conv2 = nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1)
 
-        # TODO: check this
         if out_channels != in_channels:
             self.proj = nn.Conv2d(in_channels, out_channels, kernel_size=1)  # spatial-resolution unchanged
-        # TODO: how important is the init here? Karras used nn.init.orthonogal_
+        # XXX: Karras did some special init incl. nn.init.orthonogal_
     
     def forward(self,
                 x: torch.Tensor,
@@ -78,17 +77,22 @@ class MHSelfAttention2d(CondModule):
     def __init__(self,
                  in_channels: int,
                  nb_heads: int,
-                 norm: Callable[[int], nn.Module]) -> None:
+                 norm: Callable[[int], nn.Module],
+                 pos_embed: bool=False,
+                 input_shape: tuple[int, int, int]=()) -> None:
         super().__init__()
         assert in_channels % nb_heads == 0, "q,k,v emb. dim: in_channels/nb_heads should be an integer"
         self.nb_heads, self.norm = nb_heads, norm(in_channels)
         self.qkv_proj = nn.Conv2d(in_channels, in_channels*3, kernel_size=1)
         self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)  # shape unchanged, affine transformation
-        # TODO: how important is the init here? Karras set weights and biases of conv to 0 initially
+        # XXX: Karras set weights and biases of conv to 0 initially
+        
+        self.pe = nn.Parameter(1e-2*torch.randn(1, 1, *input_shape[-2:])) if pos_embed and input_shape != () else None
 
     def forward(self,
                 x: torch.Tensor,
                 cond: torch.Tensor) -> torch.Tensor:
+        if self.pe is not None: x += self.pe
         qkv = self.qkv_proj(self.norm(x, cond))                                        # N x 3C x H x W
         qkv = qkv.view(x.shape[0], 3*self.nb_heads, x.shape[1]//self.nb_heads, -1)     # N x 3nb_heads x C//nb_heads x HW
         Q, K, V = (qkv.transpose(-1, -2)).chunk(3, dim=1)                              # N x nb_heads x HW x C//nb_heads each
@@ -98,7 +102,7 @@ class MHSelfAttention2d(CondModule):
 
 class CondUpDownBlock(CondModule):
     def __init__(self,
-                 in_channels: int,
+                 input_shape: tuple[int, int, int],
                  mid_channels: int,
                  out_channels: int,
                  cond_channels: int,
@@ -107,21 +111,22 @@ class CondUpDownBlock(CondModule):
                  self_attention: bool=True,
                  updown_state: State=State.NONE) -> None:
         super().__init__()
+        in_channels = input_shape[0]
         # All the conditioning go into the normalization layers
         self.updown_state = updown_state
         self.layers = nn.ModuleList([])
         mid = mid_channels
 
         # Downsampling/avg pooling to shrink the spatial resolution, not number of channels
-        # TODO: Karras used something else
+        # XXX: Karras used a fixed kernel convolution
         if updown_state == State.DOWN:
             self.layers.append(nn.AvgPool2d(2, 2))
         # Upsampling to increase the spatial resolution. Half also the num. of channels
-        # TODO: Karras used something else
+        # XXX: Karras used a fixed kernel convolution
         elif updown_state == State.UP:
             self.layers.append(nn.Sequential(nn.Upsample(scale_factor=2),
                                              nn.Conv2d(in_channels, in_channels//2, kernel_size=1)))
-            #TODO: check, TODO kernel_size = 3 and add padding of 1
+            # TODO kernel_size = 3 and add padding of 1
 
         for i in range(nb_layers):
             nic = in_channels if i == 0 else mid
@@ -136,7 +141,9 @@ class CondUpDownBlock(CondModule):
             if self_attention:
                 self.layers.append(MHSelfAttention2d(in_channels=noc,
                                                     nb_heads=nb_heads,
-                                                    norm=norm))
+                                                    norm=norm,
+                                                    pos_embed=True,
+                                                    input_shape=input_shape))
         
     def forward(self,
                 x: torch.Tensor,

src/models/unet.py
@@ -8,6 +8,7 @@ from .blocks import NoiseEmbedding, LabelEmbedding, CondUpDownBlock, State
 class UNet(nn.Module):
     def __init__(self,
                  image_channels: int,
+                 image_size: int,
                  in_channels: int,
                  min_channels: int,
                  depths: list[int],
@@ -32,29 +33,29 @@ class UNet(nn.Module):
         for i in range(len(depths)+1):  # +1 due to the bridge
             nic = in_channels if i == 0 else mid
             mid = min_channels if i == 0 else nic*2
+            size = image_size if i == 0 else size//2
             updown_state = State.NONE if i == 0 else State.DOWN
             self_attention = self_attentions[i] if i != len(depths) else self_attention_bridge
             idx = min(i, len(depths)-1)
-            down_blocks.append(CondUpDownBlock(in_channels=nic,
+            down_blocks.append(CondUpDownBlock(input_shape=(nic, size, size),
                                                 mid_channels=mid,
                                                 out_channels=mid,
                                                 cond_channels=cond_channels,
                                                 nb_layers=depths[idx],
                                                 self_attention=self_attention,
-                                                #self_attention=i >= start_self_attention,
                                                 updown_state=updown_state))
         for i in range(len(depths)):
             nic = 2*min_channels if i == len(depths)-1 else mid
             mid = in_channels if i == len(depths)-1 else nic//2
+            size = image_size if i == len(depths)-1 else size*2
             idx = len(depths)-i-1
             
-            up_blocks.append(CondUpDownBlock(in_channels=nic,
+            up_blocks.append(CondUpDownBlock(input_shape=(nic, size, size),
                                                 mid_channels=mid,
                                                 out_channels=mid,
                                                 cond_channels=cond_channels,
                                                 nb_layers=depths[idx],
                                                 self_attention=self_attentions[idx],
-                                                #self_attention=i < len(depths)-start_self_attention,
                                                 updown_state=State.UP))
         self.down_blocks = nn.ModuleList(down_blocks)
         self.up_blocks = nn.ModuleList(up_blocks)