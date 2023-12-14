0589982e5a7c4ebe58401636266f7980ab3bce2b
Zenchiyu <stephane.nguyen@etu.unige.ch>
2023-12-14 22:09:21 +0100

    computing fid for 10 000 generated imgs should take approx 17 min for fashionmnist uncond so should take +1hour for 50k generated imgs

--- src/fid.py
+++ src/fid.py
@@ -1,8 +1,8 @@
 import hydra
 import torch
 
+from cleanfid import fid
 from tqdm import tqdm
-# from torcheval.metrics import FrechetInceptionDistance
 from torchvision.utils import save_image 
 from init import init
 from omegaconf import DictConfig
@@ -11,11 +11,12 @@ from sampler import sample
 from utils import float2tensor
 
 
-def create_image_directories() -> tuple[Path, Path]:
+def create_image_directories(dataset_name: str) -> tuple[Path, Path]:
     """
     Create directories for saving images.
     """
-    gen_path, ref_path = Path("./data/generated/"), Path("./data/reference")
+    dataset_name = str.lower(dataset_name)
+    gen_path, ref_path = Path(f"./data/{dataset_name}/generated/"), Path(f"./data/{dataset_name}/reference/")
     gen_path.mkdir(parents=True, exist_ok=True)
     ref_path.mkdir(parents=True, exist_ok=True)
     return gen_path, ref_path
@@ -27,7 +28,7 @@ def fid(cfg: DictConfig):
     # Initialization
     init_tuple = init(cfg)
     model, diffusion, info, dl = init_tuple.model, init_tuple.diffusion, init_tuple.info, init_tuple.dl
-    gen_path, ref_path = create_image_directories()
+    gen_path, ref_path = create_image_directories(cfg.dataset.name)
 
     # Don't use the checkpoint seed for sampling
     torch.manual_seed(seed)
@@ -39,6 +40,7 @@ def fid(cfg: DictConfig):
         sampling_method = "euler"
 
     batch_size = 10
+    num_gen = 10_000  # literature: on 50_000 generated images
     N, C, H, W = batch_size, info.image_channels, info.image_size, info.image_size
 
     i = 0
@@ -48,7 +50,7 @@ def fid(cfg: DictConfig):
             i +=1
         
     i = 0
-    for _ in tqdm(range(0, 10_000, batch_size)):  # literature: on 50_000 generated images
+    for _ in tqdm(range(0, num_gen, batch_size)):
         fake_samples = sample(
                             N, C, H, model, diffusion,
                             uncond_label=info.num_classes,
@@ -62,5 +64,9 @@ def fid(cfg: DictConfig):
             save_image(float2tensor(image), gen_path / f"{i}.png")
             i +=1
     
+    score = fid.compute_fid(str(gen_path), str(ref_path))
+    print(f"FID: {score}")
+
+
 if __name__ == "__main__":
     fid()
