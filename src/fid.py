c8aac57e1c06e26938a39fbe55f8431a730d56d7
Zenchiyu <stephane.nguyen@etu.unige.ch>
2023-12-15 15:23:20 +0100

    FID for train, validation and test sets

--- src/fid.py
+++ src/fid.py
@@ -2,30 +2,33 @@ import hydra
 import torch
 
 from cleanfid import fid
-from tqdm import tqdm
-from torchvision.utils import save_image 
-from init import init
 from omegaconf import DictConfig
 from pathlib import Path
+from tqdm import tqdm
+from torchvision.utils import save_image 
+
+from data import DataLoaders
+from init import init
 from sampler import sample
 from utils import float2tensor
 
 
 def create_directories(
         dataset_name: str,
-        sampling_method: str
-    ) -> tuple[Path, Path, Path]:
+        sampling_method: str,
+        num_gen: str
+    ) -> tuple[list[Path], Path, Path]:
     """
-    Create directories for saving images and fid.
+    Create directories for saving images and fids.
     """
     dataset_name = str.lower(dataset_name)
-    ref_path = Path(f"./data/fid/{dataset_name}/reference/")
-    gen_path = Path(f"./data/fid/{dataset_name}/{sampling_method}/generated/")
+    ref_paths = [Path(f"./data/fid/{dataset_name}/reference_{split}/") for split in DataLoaders._fields]
+    gen_path = Path(f"./data/fid/{dataset_name}/{sampling_method}/generated_{num_gen//1000}k/")
     fid_path = Path(f"./results/fid/{dataset_name}/{sampling_method}/")
-    ref_path.mkdir(parents=True, exist_ok=True)
+    for ref_path in ref_paths: ref_path.mkdir(parents=True, exist_ok=True)
     gen_path.mkdir(parents=True, exist_ok=True)
     fid_path.mkdir(parents=True, exist_ok=True)
-    return ref_path, gen_path, fid_path
+    return ref_paths, gen_path, fid_path
 
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def compute_fid(cfg: DictConfig):
@@ -38,26 +41,28 @@ def compute_fid(cfg: DictConfig):
         sampling_method = cfg.common.sampling.method
     except:
         sampling_method = "euler"
-    ref_path, gen_path, fid_path = create_directories(cfg.dataset.name, sampling_method)
+    batch_size = 10
+    num_gen = 10_000  # literature: on 50_000 generated images
+    N, C, H, W = batch_size, info.image_channels, info.image_size, info.image_size
+
+    ref_paths, gen_path, fid_path = create_directories(num_gen, cfg.dataset.name, sampling_method)
 
     # Don't use the checkpoint seed for sampling
     torch.manual_seed(seed)
 
-    # Sample and display
-    batch_size = 10
-    num_gen = 50_000  # literature: on 50_000 generated images
-    N, C, H, W = batch_size, info.image_channels, info.image_size, info.image_size
-
-    # Don't save again the images from the test set.
-    if not(Path.exists(ref_path / "0.png")):
-        i = 0
-        for (images, _) in dl.test:
-            for image in images:
-                save_image(float2tensor(image), ref_path / f"{i}.png")
-                i +=1
+    # Reference sets
+    for ref_path, dataloader, split in zip(ref_paths, dl, DataLoaders._fields):
+        if not(Path.exists(ref_path / "0.png")):
+            print(f"Start saving images from {split} ref set")
+            i = 0
+            for (images, _) in tqdm(dataloader):
+                for image in images:
+                    save_image(float2tensor(image), ref_path / f"{i}.png")
+                    i +=1
     
-    # Don't generate & save if already generated & saved
+    # Generated set
     if not(Path.exists(gen_path / f"{num_gen-1}.png")):
+        print(f"Start saving generated images")
         i = 0
         for _ in tqdm(range(0, num_gen, batch_size)):
             fake_samples = sample(
@@ -73,10 +78,12 @@ def compute_fid(cfg: DictConfig):
                 save_image(float2tensor(image), gen_path / f"{i}.png")
                 i +=1
     
-    score = fid.compute_fid(str(gen_path), str(ref_path))
-    print(f"FID: {score}")
-    with open(str(fid_path / f"uncond_fid_{num_gen}.txt"), 'w') as f:
-        print(f"FID: {score}", file=f)
+    for ref_path, split in zip(ref_paths, DataLoaders._fields):
+        score = fid.compute_fid(str(gen_path), str(ref_path))  # TODO: Not optimal, it always recomputes gen statistics
+        txt = f"FID {split} {num_gen//1000}k: {score}"
+        print(txt)
+        with open(str(fid_path / f"uncond_fid_{split}_{num_gen//1000}k.txt"), 'w') as f:
+            print(txt, file=f)
 
 
 if __name__ == "__main__":
