04f0d8327368704c11db5dfd8d5c57f74064b963
Zenchiyu <stephane.nguyen@etu.unige.ch>
2023-12-16 10:07:00 +0100

    Generating bigger pictures in a convolutional manner :P. Interesting results showing some kind of local and global consistencies. We can better see it in FashionMNIST

--- src/sample_all_cond.py
+++ src/sample_all_cond.py
@@ -7,14 +7,14 @@ from utils import save
 
 from omegaconf import DictConfig
 
-
+@torch.no_grad()
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def sample_all_cond(cfg: DictConfig):
     model, diffusion, info, _, sampling_method, path, cfgscale_str = init_sampling(cfg)
     
     # Sample 90 pictures for each class and save
     nb_per_class = 3*30
-    N, C, H, W = nb_per_class*info.num_classes, info.image_channels, info.image_size, info.image_size
+    N, C, H, W = nb_per_class*info.num_classes, info.image_channels, 64, 64 # info.image_size, info.image_size
     kwargs = {
         "num_samples": N,
         "image_channels": C,
@@ -27,11 +27,11 @@ def sample_all_cond(cfg: DictConfig):
         "num_steps": cfg.common.sampling.num_steps,
         "sampling_method": sampling_method,
     }
-    samples = sample(**kwargs)
+    samples = sample(nb_chunks=30,**kwargs)
 
     save(samples.view(info.num_classes, nb_per_class, C, H, W)[:, :10].reshape(-1, C, H, W),
-         path / f"cond_10_cfgscale_{cfgscale_str}.png", nrow=10, padding=1)
-    save(samples, path / f"cond_{nb_per_class}_cfgscale_{cfgscale_str}.png", nrow=30, padding=1)
+         path / f"cond_10_cfgscale_{cfgscale_str}_64x64.png", nrow=10, padding=1)
+    save(samples, path / f"cond_{nb_per_class}_cfgscale_{cfgscale_str}_64x64.png", nrow=30, padding=1)
 
 if __name__ == "__main__":
     sample_all_cond()
